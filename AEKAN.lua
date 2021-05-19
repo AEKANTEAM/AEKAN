fce3fafb0b1557a27e90d78f377f05a5f5adbf95
AEKAN TEAM <58911326+AEKANTEAM@users.noreply.github.com>
2021-05-19 07:47:57 +0300

    Add files via upload

--- AEKAN.lua
+++ AEKAN.lua
@@ -12165,8 +12165,6 @@ local msg_id = msg.id_/2097152/0.5
 https.request("https://api.telegram.org/bot"..token..'/sendMessage?chat_id=' .. msg.chat_id_ .. '&text=' .. URL.escape(Text).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard))
 return false
 end
-end ---- Chat_Type = 'GroupBot' 
-end ---- Chat_Type = 'GroupBot' 
 if text == "تعطيل الزخرفه" and Mod(msg) then
 if AddChannel(msg.sender_user_id_) == false then
 local textchuser = database:get(bot_id..'text:ch:user')
