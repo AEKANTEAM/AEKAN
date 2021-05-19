ae2db7435f9ed7783e1fe23843337abec9ead597
AEKAN TEAM <58911326+AEKANTEAM@users.noreply.github.com>
2021-05-19 07:56:06 +0300

    Add files via upload

--- AEKAN.lua
+++ AEKAN.lua
@@ -12133,10 +12133,6 @@ ID = "OpenChat",
 chat_id_ = chat_id
 }, cb, nil)
 end
-if text == 'م1' or text == 'م2' or text == 'م3' or text == 'م4' or text == 'م5' then
-send(msg.chat_id_, msg.id_,"مرحبا . اكتب `الاوامر` وتحكم في الكيبورد الكلايش")
-return false
-end
 if text == 'رابط الحذف' or text == 'رابط حذف' then
 send(msg.chat_id_, msg.id_,"رابط الحذف في جميع مواقع التواصل ♡∶\nفكر قبل لا تتسرع وتروح\n﹊﹊﹊﹊﹊﹊﹊﹊﹊\n ♡∶ رابط حذف  [Telegram](https://my.telegram.org/auth?to=delete) ܁\n ♡∶ رابط حذف [instagram](https://www.instagram.com/accounts/login/?next=/accounts/remove/request/permanent/) ܁\n ♡∶ رابط حذف [Facebook](https://www.facebook.com/help/deleteaccount) ܁\n ♡∶ رابط حذف [Snspchat](https://accounts.snapchat.com/accounts/login?continue=https%3A%2F%2Faccounts.snapchat.com%2Faccounts%2Fdeleteaccount) ܁") 
 return false
