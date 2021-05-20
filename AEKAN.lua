de79299733de45af532533fea1f231e7ad837010
AEKAN TEAM <58911326+AEKANTEAM@users.noreply.github.com>
2021-05-20 15:32:02 +0300

    Add files via upload

--- AEKAN.lua
+++ AEKAN.lua
@@ -13183,7 +13183,7 @@ keyboard.inline_keyboard = {
 return https.request("https://api.telegram.org/bot"..token..'/editMessageText?chat_id='..Chat_id..'&text='..URL.escape(Teext)..'&message_id='..msg_idd..'&parse_mode=markdown&disable_web_page_preview=true&reply_markup='..JSON.encode(keyboard)) 
 end
 if DAata == '/help5' then
-if not Mod(data) then
+if not Sudo(data) then
 local notText = '♡ عذرا الاوامر هذه لا تخصك'
 https.request("https://api.telegram.org/bot"..token.."/answerCallbackQuery?callback_query_id="..data.id_.."&text="..URL.escape(notText).."&show_alert=true")
 return false
