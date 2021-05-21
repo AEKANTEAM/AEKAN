00e9521ab5baabceed089a7cff3ef0edf5b9cfb7
AEKAN TEAM <58911326+AEKANTEAM@users.noreply.github.com>
2021-05-22 02:47:05 +0300

    Add files via upload

--- AEKAN.lua
+++ AEKAN.lua
@@ -804,7 +804,6 @@ if type == "mute" then
 Reply_Status(msg,msg.sender_user_id_,"reply","♡∶ قام بالتكرار هنا وتم كتمه")  
 database:sadd(bot_id.."Muted:User"..msg.chat_id_,msg.sender_user_id_) 
 return false  
-end
 end  
 function plugin_mjnonh(msg)
 for v in io.popen('ls File_Bot'):lines() do
